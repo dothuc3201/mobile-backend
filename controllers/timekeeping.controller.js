09148bbec9f168da8d1e577b4d42a0cc88fed59f
hoikhongtuyen <hoikhongtuyen@gmail.com>
2022-05-23 16:25:22 +0700

    fix

--- controllers/timekeeping.controller.js
+++ controllers/timekeeping.controller.js
@@ -148,8 +148,8 @@ const getThongtinchamcongBydate = async (req, res) => {
     const lichsu = await TimekeepingInfo.findOne({
       where: {
         createdAt: {
-          [Op.lt]: date,
-          [Op.gt]: new Date(date.getFullYear, date.getMonth, date.getDate, 23),
+          [Op.lt]: new Date(date.getFullYear, date.getMonth, date.getDate, 23),
+          [Op.gt]: date,
         },
         user_id: user.id,
       },
